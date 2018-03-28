e0dc1b1d8c32605e321e5299104b4dcb7b1cddc4
CLooker <chad.looker@gmail.com>
2018-03-28 13:07:25 -0400

    deleted picture, added links for feeling lucky option

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,22 +1,24 @@
 $(document).ready(function() {
-	$(".getRandomProject").on("click", getRandomProject);
+	$('.getRandomProject').on('click', getRandomProject);
 });
 
 const projectArr = [
-	"https://github.com/CLooker/StarWarsRPG",
-	"https://github.com/CLooker/week-4-game",
-	"https://github.com/CLooker/liri-node-app",
-	"https://github.com/CLooker/Eloquent_JS_Solutions",
-	"https://github.com/CLooker/TriviaGame",
-	"https://github.com/CLooker/Train-Scheduler",
-	"https://github.com/CLooker/Friend-Finder",
-	"https://github.com/CLooker/burger",
-	"https://github.com/CLooker/If-McDonalds-Fits-Your-Calories",
-	"https://github.com/CLooker/Hangman-Game",
-	"https://github.com/CLooker/Giphy-Generator",
-	"https://github.com/CLooker/bamazon",
-	"https://github.com/CLooker/If-McDonalds-Fits-Your-Calories",
-	"https://github.com/CLooker/udacity-react-project1"
+	'https://github.com/CLooker/StarWarsRPG',
+	'https://github.com/CLooker/week-4-game',
+	'https://github.com/CLooker/liri-node-app',
+	'https://github.com/CLooker/TriviaGame',
+	'https://github.com/CLooker/Train-Scheduler',
+	'https://github.com/CLooker/Friend-Finder',
+	'https://github.com/CLooker/burger',
+	'https://github.com/CLooker/If-McDonalds-Fits-Your-Calories',
+	'https://github.com/CLooker/Hangman-Game',
+	'https://github.com/CLooker/Giphy-Generator',
+	'https://github.com/CLooker/bamazon',
+	'https://github.com/CLooker/If-McDonalds-Fits-Your-Calories',
+	'https://github.com/CLooker/udacity-react-project1',
+	'https://github.com/CLooker/4clojure-solutions',
+	'https://github.com/CLooker/open-weather-dashboard',
+	'https://github.com/CLooker/HackerRank'
 ];
 
 function getRandomProject() {
