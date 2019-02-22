ab7bba745d68e3ba959fff6ef0d36bdf1e6c79f9
CLooker <chad.looker@gmail.com>
2019-02-22 11:27:49 -0500

    Remove old projects, Update old ones

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,31 +1,30 @@
 $(document).ready(function() {
-	$('.getRandomProject').on('click', getRandomProject);
+  $('.getRandomProject').on('click', getRandomProject);
 });
 
 const projectArr = [
-	'https://github.com/CLooker/StarWarsRPG',
-	'https://github.com/CLooker/week-4-game',
-	'https://github.com/CLooker/liri-node-app',
-	'https://github.com/CLooker/TriviaGame',
-	'https://github.com/CLooker/Train-Scheduler',
-	'https://github.com/CLooker/Friend-Finder',
-	'https://github.com/CLooker/burger',
-	'https://github.com/CLooker/If-McDonalds-Fits-Your-Calories',
-	'https://github.com/CLooker/Hangman-Game',
-	'https://github.com/CLooker/Giphy-Generator',
-	'https://github.com/CLooker/bamazon',
-	'https://github.com/CLooker/If-McDonalds-Fits-Your-Calories',
-	'https://github.com/CLooker/udacity-react-project1',
-	'https://github.com/CLooker/4clojure-solutions',
-	'https://github.com/CLooker/open-weather-dashboard',
-	'https://github.com/CLooker/HackerRank',
-	'https://github.com/CLooker/react-redux-readable'
+  'https://github.com/CLooker/StarWarsRPG',
+  'https://github.com/CLooker/liri-node-app',
+  'https://github.com/CLooker/TriviaGame',
+  'https://github.com/CLooker/Train-Scheduler',
+  'https://github.com/CLooker/Friend-Finder',
+  'https://github.com/CLooker/burger',
+  'https://github.com/CLooker/If-McDonalds-Fits-Your-Calories',
+  'https://github.com/CLooker/Hangman-Game',
+  'https://github.com/CLooker/Giphy-Generator',
+  'https://github.com/CLooker/bamazon',
+  'https://github.com/CLooker/If-McDonalds-Fits-Your-Calories',
+  'https://github.com/CLooker/my-reads',
+  'https://github.com/CLooker/4clojure-solutions',
+  'https://lit-ridge-37329.herokuapp.com/',
+  'https://github.com/CLooker/HackerRank',
+  'https://github.com/CLooker/react-redux-readable'
 ];
 
 function getRandomProject() {
-	window.open(projectArr[getRandomInt(0, projectArr.length - 1)]);
+  window.open(projectArr[getRandomInt(0, projectArr.length - 1)]);
 }
 
 function getRandomInt(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
